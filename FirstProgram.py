09bf465ce91abff93a6265ff527791c5f5e831dd
Iqra Mulla <iqramulla0605@gmail.com>
2026-07-31 12:34:49 +0530

    Your commit message

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,4 +1,22 @@
-name=("Iqra")
-age=(21)
-print("My name is", name, "and I am", age, "years old.")
-print(type(age))
+#Sum
+a=2
+b=7
+sum=a+b
+print(sum)
+#Subtraction
+c=10
+d=5
+sub=c-d
+print(sub)
+
+#Division
+e=20
+f=5
+div=e/f
+print(div)
+
+#prodcuct
+g=4
+h=8
+prod=g*h
+print(prod)
