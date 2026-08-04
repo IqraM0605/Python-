dcb28745d50520ba0d770172d0657c84bbfebe6f
Iqra Mulla <iqramulla0605@gmail.com>
2026-08-04 16:33:12 +0530

    grades

--- conditional_statement.py
+++ conditional_statement.py
@@ -1,5 +1,10 @@
-age=10
-if(age>=18):
-    print("Can vote")
+mark=int(input("Enter your marks: "))
+if (mark>=90):
+    print("A")
+elif (mark>=80 and mark<90):
+    print("B")
+elif (mark>=70 and mark<80):
+    print("C")
 else:
-    print("Cannot vote")
+    grade="F"
+print("Grade is:", grade)
