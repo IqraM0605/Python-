0b0c9ddf7c24c919ae8eaa91745c8b86067369b5
Iqra Mulla <iqramulla0605@gmail.com>
2026-08-04 20:11:51 +0530

    greater number

--- practiceQ.py
+++ practiceQ.py
@@ -1,7 +1,19 @@
-while True:
-    num = int(input("Enter number: "))
+# while True:
+#     num = int(input("Enter number: "))
 
-    if num % 2 == 0:
-        print("Even")
-    else:
-        print("Odd")
+#     if num % 2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
+
+a = int(input("Enter first number: "))
+
+b = int(input("Enter second number: "))
+
+c = int(input("Enter third number: "))
+if(a>= c) and a >= c:
+    print("a is greater")
+elif(b >= c ):
+    print("b is greater")
+else:
+    print("c is greater")
