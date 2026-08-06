252a4f4e0d3352037a6fe77bc96de27abafda167
Iqra Mulla <iqramulla0605@gmail.com>
2026-08-06 11:50:00 +0530

    nested dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,21 @@
-info = {
-    "key" : "value" ,
-    "name" : "Iqra" ,
-    "learning" : "Python" ,
-    "cgpa" : "8.70" ,
-    "feeling" : "sad"
+# info = {
+#     "key" : "value" ,
+#     "name" : "Iqra" ,
+#     "learning" : "Python" ,
+#     "cgpa" : "8.70" ,
+#     "feeling" : "sad"
+# }
+# info["name"] = "UmmeHANI"
+# print(info)
+
+
+#NESTED DICTIONAY
+student = {
+"name ": "Iqra" ,
+"subject" : {
+    "sub1" : "Python" ,
+    "sub2" : "Java" ,
+    "sub3" : "C++"
 }
-info["name"] = "UmmeHANI"
-print(info)
+}
+print(student)
